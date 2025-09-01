a16c85ad8a2c524ba2caa46319607e18d5822ff2
Faharel <Fahareladitthapratama@gmail.com>
2025-09-01 17:00:09 +0700

    feat(js): add array basics with push, pop, shift, and unshift

---
+++ dea-afrizal/array.js
@@ -0,0 +1,7 @@
+let namaSiswa = [ 'JP', 'BgDik', 'BgGik' ]; // Array adalah tipe data yang bisa menyimpan banyak nilai, seperti contoh ini array menyimpan 3 nilai
+// sama saja seperti sebelumnya, variable bisa dibuat kosong, dan bisa juga ditambahkan nilainya setelahnya, menggunakan Variable[0] = 'Nilai'
+namaSiswa.push('Farel', 'Fikri') // Menambahkan nilai pada array yang sudah ada
+namaSiswa.shift() // Menghapus nilai pertama
+namaSiswa.pop() // Menghapus nilai terakhir
+
+alert(namaSiswa) // Bisa juga hanya memanggil 1 nilai berdasarkan urutan nya, tapi perlu di ingat dalam pemrogramman urutan (Index) dimulai dari angka 0 bukan 1
